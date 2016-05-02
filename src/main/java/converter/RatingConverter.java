d41a8c0facc1b5228bc33d552b5d78cac4630f44
Adam Wasilczuk <epiuqe@gmail.com>
2016-05-02 13:38:12 +0200

    add rating convereter

---
+++ src/main/java/converter/RatingConverter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ *
+ * @author Adam Wasilczuk
+ */
+@FacesConverter("RatingConverter")
+public class RatingConverter  implements Converter{
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        return o.toString() + "/10";
+    }
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
